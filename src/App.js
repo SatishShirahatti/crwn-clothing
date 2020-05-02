bcc0b7715f2839a6a44ba918c7997714d6a02ffe
satishshirahatti <shirahatti.satish2010@gmail.com>
2020-05-02 23:00:26 +0200

    breaking the components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Homepage from "./pages/homepage/homepage.component"
+
 function App() {
 	return (
 		<div className="App">
-			hello world
+			<Homepage />
 		</div>
 	);
 }
