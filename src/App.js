26acd5dddb5e7cc4f0cf4c03d5bd063c284e5a9e
satishshirahatti <shirahatti.satish2010@gmail.com>
2020-05-03 20:18:29 +0200

    added firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/fiirebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import setCurrentUser from "./redux/user/user.actions"
 
 class App extends React.Component {
